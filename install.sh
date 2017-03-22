142f3abf5b45123ed556ebd1f6e15196c89632cb
Rup Gautam <RupGautam@users.noreply.github.com>
2017-03-21 23:44:30 -0500

    echo colors

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@
 
 #Check if brew is installed and update if 'yes'
 if type brew > /dev/null; then
-  echo "Homebrew Exists";
+  echo "\e[36mHomebrew Exists";
 else
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 fi;
-brew update && brew upgrade && brew cleanup;
+brew update && cleanup;
 brew install zsh zsh-completions;
 
 #Change shell to zsh
@@ -26,12 +26,12 @@ cp ~/temp/OSX-Setup/.gitconfig ~/.gitconfig;
 curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh;
 cp -r ~/temp/OSX-Setup/.oh-my-zsh ~ ; #copying my entire folder
 cp ~/temp/OSX-Setup/.zshrc ~/.zshrc;
-echo "installed oh-my-zsh and .zshrc config file copied successfully";
+echo "\e[36mInstalled oh-my-zsh and .zshrc config file copied successfully";
 
 #install and configure atom-ide
 brew install atom;
 cp -r ~/temp/OSX-Setup/.atom ~ ;
-echo "atom installed and config files copied successfully!";
+echo "\e[36mAtom installed and config files copied successfully!";
 
 # Clone setup repo
 cd ~/temp;
@@ -44,37 +44,37 @@ cp -r ~/temp/OSX-Setup/.nano ~ ;
 cp ~/temp/OSX-Setup/.nanorc ~/.nanorc;
 cp ~/temp/OSX-Setup/.alias ~/.alias;
 cp ~/temp/OSX-Setup/.screenrc ~/.screenrc;
-echo "All necessary dotfiles has been copied successfully!";
+echo "\e[36mAll necessary dotfiles has been copied successfully!";
 
 
 # backup origin vimrc file
 [[ -f ~/.vimrc-bak ]] || cp ~/.vimrc ~/.vimrc-bak;
 mv ~/temp/OSX-Setup/.vimrc ~/.vimrc;
-echo ".vimrc backed-up old one and copied fresh one successfully!";
+echo "\e[36m.vimrc backed-up old one and copied fresh one successfully!";
 
 # vim pulgin controller - vundle
 [[ -d ~/.vim/bundle/Vundle.vim ]] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim;
-echo "VundleVim installed successfully!";
+echo "\e[36mVundleVim installed successfully!";
 
 # colors schemes
 cd ~/temp;
 [[ -d ~/temp/vim-colorschemes ]] || git clone https://github.com/flazz/vim-colorschemes.git;
 [[ -d ~/.vim/colors ]] || mv ~/temp/vim-colorschemes/colors ~/.vim/;
-echo "Vim vim-colorschemes installed successfully!";
+echo "\e[36mVim vim-colorschemes installed successfully!";
 
 # fonts for airline
 cd  ~/temp;
 [[ -d ~/temp/fonts ]] || git clone https://github.com/powerline/fonts.git;
-echo "You might have to provide admin password to install powerline fonts";
+echo "\e[36mYou might have to provide admin password to install powerline fonts";
 cd fonts;
 sudo sh ./install.sh;
-echo "powerline fonts installed successfully!";
+echo "\e[36mPowerline fonts installed successfully!";
 
 # ack supported
 brew install ack ag;
 
 # YouCompleteMe supported
-echo "installing YouCompleteMe vim plugin, this might takes time";
+echo "\e[36mInstalling YouCompleteMe vim plugin, this might takes time";
 if [[ -d ~/.vim/bundle/YouCompleteMe ]]; then
   echo "YouCompleteMe Exists";
 else
@@ -84,7 +84,7 @@ else
   # for nodejs
   ./install.py --tern-completer;
 fi;
-echo "Done, installing YouCompleteMe successfully!";
+echo "\e[36mDone, installing YouCompleteMe successfully!";
 
 
 # update vim, replace the origin
@@ -97,4 +97,11 @@ vim +PluginInstall! +qall;
 rm -rf ~/temp;
 
 #Manual setting notice.
-echo "Please install Sublime Text and manually copy the Preferences setting"
+echo "\e[36mPlease install Sublime Text and manually copy the Preferences setting"
+
+
+#Please update brew and clean-up 
+echo "\n"
+echo -e "\e[36mPlease update....\n\e[92mbrew update && brew upgrade && brew clean"
+echo -e "\e[36mIt might time, So Please wait.."
+
